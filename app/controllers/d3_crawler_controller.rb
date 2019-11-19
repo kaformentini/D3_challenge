bf10ebb2d67641bead121b54bae3158c1d87a685
Kah <kaformentini@gmail.com>
2019-11-19 14:30:31 -0300

    código mais legivel

--- app/controllers/d3_crawler_controller.rb
+++ app/controllers/d3_crawler_controller.rb
@@ -2,14 +2,13 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'uri'
+
 class D3CrawlerController < ApplicationController
   def index
   end
 
   def result
-    # @result = {messages: params}
     @result_json = crawler(params[:crawler][:url])
-    # byebug
     puts @result_json.length
     render json: @result_json, status: :ok
   end
@@ -27,8 +26,8 @@ class D3CrawlerController < ApplicationController
   end
 
 
-  def show_links(to_read_links, found_links, result_map)
-    while to_read_links.any?
+def show_links(to_read_links, found_links, result_map)
+  while to_read_links.any?
       @current_url = to_read_links[0]
       to_read_links.delete(@current_url)
       statics_link = find_assets(@current_url)
@@ -37,66 +36,64 @@ class D3CrawlerController < ApplicationController
       found_links << @current_url
       puts @current
       result_map << {@current_url => statics_link}
-    
-
-    end
+  end
 end
 
 def add_links_to_read_list(raw_url, to_read_links, found_links)
-    begin
-        url = parse_url(raw_url)
-        if url == false
-            return
-        end
-        
-        doc = Nokogiri::HTML(open(url))
-        current_url_links = doc.css('a[href]').map{|a| a['href']}
-
-        current_url_links.each do |link|
-                                   
-            link = parse_url(link)
-            
-            if link && pre_conditions(link, to_read_links, found_links)
-                to_read_links << link
-                
-            end
-        end
-    
-    rescue => exception
-        return
+  begin
+    url = parse_url(raw_url)
+    if url == false
+      return
     end
+    
+    doc = Nokogiri::HTML(open(url))
+    current_url_links = doc.css('a[href]').map{|a| a['href']}
+
+    current_url_links.each do |link|
+                                
+      link = parse_url(link)
+      
+      if link && pre_conditions(link, to_read_links, found_links)
+        to_read_links << link
+      end
+    end
+  
+  rescue => exception
+    return
+  end
 end
 
 def find_assets(current_url)
-    begin
-        doc = Nokogiri::HTML(open(@current_url))
-        link = doc.css('a[href]').map{|a| a['href']}
-        return doc.css('link[href], script[src], img[src]').map{|elm| elm['href'] || elm['src']}
-    rescue => exception
-        return
-    end
+  begin
+    doc = Nokogiri::HTML(open(@current_url))
+    link = doc.css('a[href]').map{|a| a['href']}
+    return doc.css('link[href], script[src], img[src]').map{|elm| elm['href'] || elm['src']}
+  rescue => exception
+    return
+  end
 end
 
 def pre_conditions(link, to_read_links, found_links)
-    if to_read_links.include? link 
-        return false
-    end
-    if found_links.include? link
-        return false
-    end
+  if to_read_links.include? link 
+    return false
+  end
+
+  if found_links.include? link
+    return false
+  end
     return true
 end
 
 def parse_url(current_url)
-    uri = URI(current_url)
+  uri = URI(current_url)
 
-    if uri.host == "mineatlas.com"
-        return uri.to_s
-    end
+  if uri.host == "mineatlas.com"
+    return uri.to_s
+  end
 
-    if uri.host.nil? && (not uri.path.nil?)
-        return URI.join("http://mineatlas.com/", uri.path).to_s
-    end
+  if uri.host.nil? && (not uri.path.nil?)
+    return URI.join("http://mineatlas.com/", uri.path).to_s
+  end
     return false
 end
   
